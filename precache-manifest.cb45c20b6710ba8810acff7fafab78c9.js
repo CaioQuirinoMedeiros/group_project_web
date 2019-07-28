a389d13eb292428c1a62090bb58755cffc7d5205
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-28 19:06:44 -0300

    Updates

---
+++ precache-manifest.cb45c20b6710ba8810acff7fafab78c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d61453959074f99488492b2e2a3a7d",
+    "url": "/group_project_web/index.html"
+  },
+  {
+    "revision": "888af52a4956ea47ef3d",
+    "url": "/group_project_web/static/css/2.19b13f22.chunk.css"
+  },
+  {
+    "revision": "888af52a4956ea47ef3d",
+    "url": "/group_project_web/static/js/2.c8eee3eb.chunk.js"
+  },
+  {
+    "revision": "f698c09e0a2a85fa667e",
+    "url": "/group_project_web/static/js/main.23eeaf4a.chunk.js"
+  },
+  {
+    "revision": "db2c3cda67dbf87b48de",
+    "url": "/group_project_web/static/js/runtime~main.5594a660.js"
+  }
+]);
